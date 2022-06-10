0db1af293c150282fd7d3a5e9f07403445286746
Jerry <jerry_yang_2@outlook.com>
2022-06-09 22:16:06 -0700

    Added save/load feature

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Hangman
-Hangman game from Odin Project
+Hangman game from Odin Project. In order to play, please download onto desktop and change the directory in the code.
--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,6 @@
 class Game
     def initialize
+        #change directory if you're downloading from GitHub
         @text_file = File.open(File.expand_path("/home/jerry/repos/Hangman/google-10000-english-no-swears.txt"))
         @available_words = []
         File.readlines(@text_file).each do |line|
@@ -13,25 +14,52 @@ class Game
     end
 
     def selected_word
-        puts "#{@selected_word}, #{@selected_word.length}"
+        puts "#{@selected_word}\n#{@selected_word.length}"
     end
 
     def chosen_word
         @chosen_word = @selected_word.to_s.split('')
     end
 
+    def save_game
+        #change directory if you're downloading from GitHub
+        @save_file = File.open(File.expand_path("/home/jerry/repos/Hangman/savegames/savefile.txt"), 'w')
+        @playerguess = @player_guess.join('')
+        @chosenword = @chosen_word.join('')
+        @save_file.puts("#{@playerguess}\n#{@chosenword}\n#{@count}")
+        @save_file.close
+    end
+
     def game_start
+        @text_file.close
         chosen_word
         @player_guess = []
-        @count = 0
+        @count = 1
         for i in @chosen_word
             @player_guess.append("_")
         end
-        until @count >= 10 do
+        until @count >= 20 do
             puts "The word is #{@chosen_word.length} characters long.\n"
-            print "Please enter your guess. You are on turn #{@count+1}. Your current guess is: #{@player_guess}.\n"
+            print "Enter your guess. You're on turn #{@count}. Enter 's' if you wish to save your progress, or 'l' to load last save. Your current guess is: #{@player_guess}.\n"
             @current_guess = gets().chomp.to_s
-            if @current_guess.split('').length != @chosen_word.length
+            if @current_guess == "s" || @current_guess == "S"
+                save_game
+                redo
+            elsif @current_guess == 'l' || @current_guess == "L"
+                puts "Preceeding to load last save"
+                @temp_array = []
+                #change directory if you're downloading from GitHub
+                @save_file = File.open(File.expand_path("/home/jerry/repos/Hangman/savegames/savefile.txt"), 'r')
+                File.readlines(@save_file).each do |line|
+                    line.to_s.chomp
+                    @temp_array.append(line.chomp)
+                end
+                @player_guess = @temp_array[0].split('')
+                @chosen_word = @temp_array[1].split('')
+                @count = @temp_array[2]
+                @save_file.close
+                redo
+            elsif @current_guess.split('').length != @chosen_word.length
                 puts "You did not enter the right length, try again."
                 redo
             end
@@ -42,8 +70,8 @@ class Game
                 end
             end
             if @current_guess == @chosen_word
-                puts "You win! The word was #{@selected_word}!"
-                @count = 10
+                puts "You win! The word was #{@chosen_word.join('')}!"
+                @count = 999
             end
             @count += 1
         end
